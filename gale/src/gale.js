97420f77ee7bb72e1ec045956a1aaab172762bb1
张俊 <zhangjun01@hesaitech.com>
2023-05-16 11:41:02 +0800

    config

--- gale/src/gale.js
+++ gale/src/gale.js
@@ -1,21 +1,10 @@
 import Phaser from 'phaser';
 
-import MainScene from './scene/main';
+import config from './config';
 
 class GaleGame extends Phaser.Game {
   constructor() {
-    super({
-      type: Phaser.AUTO,
-      width: 800,
-      height: 600,
-      physics: {
-        default: 'arcade',
-        arcade: {
-          gravity: { y: 200 }
-        }
-      },
-      scene: [MainScene]
-    });
+    super(config);
   }
 }
 
